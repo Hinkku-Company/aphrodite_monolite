f12205e56f0ae367317d1003482fe1363fa98a0a
Sifaconer <k.nassifacosta@gmail.com>
2023-10-23 02:52:51 -0500

    fix migration db

--- src/db/postgres/migrations/20230910033233_ddl_database.sql
+++ src/db/postgres/migrations/20230910033233_ddl_database.sql
@@ -1,15 +1,5 @@
 -- +goose Up
 -- +goose StatementBegin
-CREATE DATABASE aphrodite
-    WITH
-    OWNER = postgres
-    ENCODING = 'UTF8'
-    CONNECTION LIMIT = -1
-    IS_TEMPLATE = False;
-
-COMMENT ON DATABASE aphrodite
-    IS 'Aphrodite Monolite';
-
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 -- +goose StatementEnd
 
